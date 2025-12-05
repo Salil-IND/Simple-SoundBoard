62069dbb8f4de7b22ac950e5d45d44ac7099b012
Salil-IND <salil_programming@outlook.com>
2025-12-06 00:29:57 +0530

    Added more buttons and added Event Listeners to all of them

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const btn = document.querySelector(".button")
-const aud = document.querySelector("audio")
+// const btn = document.querySelector(".button")
+// const aud = document.querySelector("audio")
 
-btn.addEventListener("click", function(){
-    aud.pause();
-    aud.play();
+// btn.addEventListener("click", function(){
+//     aud.pause();
+//     aud.play();
+// })
+
+const btnList = document.querySelectorAll(".button")
+
+btnList.forEach(function(btn){
+    const btnID = btn.classList[1]
+    const aud = document.querySelector(`audio.${btnID}`)
+    btn.addEventListener("click", function(){
+        aud.play()
+    })
 })
