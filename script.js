4d1d7b1179f99ed962d4cfb36808c87d7a88815d
Salil-IND <salil_programming@outlook.com>
2025-12-06 22:49:01 +0530

    Added Volume Slider and made the Search Feature Functional

--- script.js
+++ script.js
@@ -7,11 +7,35 @@
 // })
 
 const btnList = document.querySelectorAll(".button")
+const srchBtn = document.querySelector(".mag-glass-cont")
+const volSlider = document.querySelector(".vol-slider")
 
 btnList.forEach(function(btn){
     const btnID = btn.classList[1]
     const aud = document.querySelector(`audio.${btnID}`)
     btn.addEventListener("click", function(){
         aud.play()
+        aud.volume = newVol
+    })
+
+})
+volSlider.addEventListener("input", function(){
+    const newVol = Number(volSlider.value)
+    document.querySelectorAll("audio").forEach(function(audio){
+        audio.volume = newVol
     })
 })
+srchBtn.addEventListener("click", function(){
+    const srchText = document.querySelector("textarea").value
+
+    const titleList = document.querySelectorAll(".title")
+    titleList.forEach(function(title){
+        if (!title.innerText.includes(srchText)){
+            title.parentElement.style.display = "none"
+        } else {
+            title.parentElement.style.display = "flex"
+        }
+    })
+})
+
+
